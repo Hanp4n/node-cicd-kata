fca9694c169c16363d90b299916affd4ec2e742a
José Fco Rodríguez Hernández <josefco.rodriguez@leanmind.es>
2026-09-25 10:47:57 +0100

    feat: add errors

--- src/application/get-dice-roll.ts
+++ src/application/get-dice-roll.ts
@@ -1,4 +1,5 @@
 export function getDiceRoll(random: () => number = Math.random): number {
+  const unusedVariable = 'fix me';
   const lintDemo:any='fix me';
   return Math.floor(random() * 6) + 1;
 }
